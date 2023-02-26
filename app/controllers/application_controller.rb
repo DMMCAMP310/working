bcbaafa9ba549050a73b6f401f37e2d8974430fb
DMMCAMP310 <ec2-user@ip-172-31-41-167.ap-northeast-1.compute.internal>
2023-02-26 14:37:31 +0000

    [commit2]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters,if: :devise_controller?
   
+  def affter_sign_in_path_for(resorce)
+    root_path
+  end
+  
+  def affter_sign_out_path_for(resorce)
+    root_path
+  end
+  
   protected
   
   def configure_permitted_parameters
