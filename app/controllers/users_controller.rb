3e75faf502aca81f816594b31ef330f1cf3d9614
DMMCAMP310 <ec2-user@ip-172-31-41-167.ap-northeast-1.compute.internal>
2023-02-26 16:39:12 +0000

    [commit3]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,25 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
+  before_action :set_user
   def edit
+    @user = current_user
   end
+
+  def update
+    @user = current_user
+    @user.update_without_password(user_params)
+    redirect_to mypage_users_url
+  end
+
+  def mypage
+    @user = current_user
+  end
+  
+  private
+    def set_user
+      @user = current_user
+    end
+    
+    def user_params
+      params.permit(:name, :email)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   root :to => 'homes#top'
-  resources :users, only: [:show, :edit, :index,]
+  resource :users, only: [:edit, :update] do
+    collection do
+      get "mypage", :to => "users#mypage"
+      get "mypage/edit", :to => "users#edit"
+      put "mypage", :to => "users#update"
+    end
+  end
 end
