bfb581620a6926be46401ce3caa50b3f665b9072
DMMCAMP310 <kuon6173@gmail.com>
2023-03-08 11:51:46 +0000

    [トップ画面]

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,21 @@
 class HomesController < ApplicationController
   def top
+    if sort_params.present?
+      @category = Category.request_category(sort_params[:sort_category])
+      @products = Product.sort_products(sort_params, params[:page])
+    elsif params[:category].present?
+      @category = Category.request_category(params[:category])
+      @products = Product.category_products(@category, params[:page])
+    else
+      @products = Product.display_list(params[:page])
+    end
+ 
+    @major_category_names = Category.major_categories
+    @categories = Category.all
   end
+  
+  private
+    def sort_params
+      params.permit(:sort, :sort_category)
+    end
 end
